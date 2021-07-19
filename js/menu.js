528d555eb0f260544d520971a082133d990ae8f4
Nivekd11 <kevinsandoval06@gmail.com>
2021-07-19 02:06:51 -0500

    Mi PortFolio

--- js/menu.js
+++ js/menu.js
@@ -7,7 +7,32 @@ window.onload=()=>{
         navMenu.classList.toggle("nav-menu-muestra");
     })
 
-}
+    }
+
+    let ubicacionPrincipal=window.pageYOffset;
+    let ubicacionCero=window.pageYOffset;
+    window.onscroll=()=>{
+        let desplazamiento=window.pageYOffset;
+        if(desplazamiento>=ubicacionCero+10){
+            document.getElementById('navegacion').style.height='10vh';
+            
+        }
+        else if(window.innerWidth>1024){
+            document.getElementById('navegacion').style.height='15vh';
+            
+        }
+        if(ubicacionPrincipal>= desplazamiento){
+            document.getElementById('navegacion').style.top='0';
+
+        }
+        
+        else{
+            document.getElementById('navegacion').style.top='-15vh';
+        }
+
+        ubicacionPrincipal=desplazamiento;
+    }
+
 
 
 }
